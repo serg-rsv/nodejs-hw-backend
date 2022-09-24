849aec9ca395fa45813fdb4ffe152e41659b1aff
Serhii Rybak <rybaksw@gmail.com>
2022-09-24 23:20:44 +0300

    install dotenv, connect DB

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const { connect } = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT);
+  })
+  .then(() => {
+    console.log('Server running. Use our API on port: 3000');
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
